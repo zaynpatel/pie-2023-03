a704d63c09176812d817e4bcb11abc4393976081
Sohum-Prime <sohumhk04@gmail.com>
2023-11-26 01:50:29 -0500

    Setup Homepage and formatting for website

---
+++ cern/my-app/src/components/AboutUs.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+function AboutUs() {
+    return (
+        <div className="main-content">
+            <h1>About Us</h1>
+            <div>
+                <h2>Sohum K</h2>
+                <p>Role: E:Robo - Jack of all GOATs</p>
+            </div>
+            <div>
+                <h2>Kevin L</h2>
+                <p>Role: ECE:Quantum - Controls and Physics</p>
+            </div>
+            <div>
+                <h2>Venkadesh E</h2>
+                <p>Role: ECE - Induction and Deduction</p>
+            </div>
+            <div>
+                <h2>Angela H</h2>
+                <p>Role: ME - Nuts Bolts and Motors, ME: Oh My!</p>
+            </div>
+            <div>
+                <h2>Ari B</h2>
+                <p>Role: E:IDK/Team Lead - Electronics and PCB</p>
+            </div>
+        </div>
+    );
+}
+
+export default AboutUs;
---
+++ cern/my-app/src/components/Electrical.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Electrical() {
+    return (
+        <div className="main-content">
+            <h1>Electrical</h1>
+            <p>Details on the PCB development, breadboard prototyping, and electronic systems like the IMU, power lockout circuit, and charging station oscillator.</p>
+            {/* Add more content as per your project progress */}
+        </div>
+    );
+}
+
+export default Electrical;
